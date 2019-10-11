f46e4dbb8fc9cc9dbdce7a0a57b19d8067e30868
Danil Kadukov <25lotov@gmail.com>
2019-10-11 19:44:00 +0300

    досашнее задание на java1

---
+++ MyClass.java
@@ -0,0 +1,7 @@
+//Данил Кадуков
+//Домашнее задание:
+// 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+public class MyClass {
+    public static void main(String[] args){
+    }
+}
---
+++ Type.java
@@ -0,0 +1,22 @@
+//2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+public class Type {
+    public static void main(String[] args){
+        byte b = 15;
+        short s = 234;
+        int i = 23434;
+        long l = 6700L;
+        float f = 43.5f;
+        double d = 33.77;
+        boolean bool = false;
+        char c = 'h';
+
+       System.out.println(b);
+       System.out.println(s);
+       System.out.println(i);
+       System.out.println(l);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println(bool);
+       System.out.println(c);
+    }
+}
