15af8633dad579903e7063e19195aa679b79ecf7
Danil Kadukov <25lotov@gmail.com>
2019-10-18 00:55:36 +0300

    q

--- src/less2/Z2.java
+++ src/less2/Z2.java
@@ -1,3 +1,5 @@
+package less2;
+
 //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
 public class Z2 {
     public static void main(String[] args) {
